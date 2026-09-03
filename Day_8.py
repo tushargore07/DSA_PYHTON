bc75b166bb32466266630cc1705dc00ee5042e83
tushargore07 <tushargore06@gmail.com>
2026-09-03 23:57:43 +0530

    ..

--- Day_8.py
+++ Day_8.py
@@ -1,1 +1,23 @@
 #@    RECURSIONS USING  PARAMETERS 
+
+
+def func(x,n):
+    if n==0:
+        return
+    print(x)
+    func(x,n-1)
+
+
+func(15,4)   
+
+
+
+
+
+def func2(i,n):
+    if i>n:
+        return
+    print(i)
+    func2(i+1,n)
+
+func2(1,4)
